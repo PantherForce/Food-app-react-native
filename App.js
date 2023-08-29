3e62d300f966ef2d90d13f419129eac62a12d4ec
likhithreddy28 <likhithreddy150@gmail.com>
2023-08-30 01:07:42 +0530

    Homepage

--- App.js
+++ App.js
@@ -1,11 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Text, View, Image,  TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Food App</Text>
-      <StatusBar style="auto" />
+      <Image 
+        source={require("./assets/wc.png")}
+
+      />
+      <Text style={{
+        paddingTop: 24,
+        fontSize: 14,
+        color: '#f96163',
+        fontWeight: 'bold',
+        fontFamily: '',
+
+      }}>
+        40 + restro serving for you
+      </Text>
+
+      <Text style={{
+        fontSize: 33,
+        fontWeight: 'bold',
+        paddingTop: 20,
+      }}>
+        Eat like a king
+      </Text>
+     
+     <TouchableOpacity style = {{
+      backgroundColor : '#f96163' ,
+      borderRadius : 12 ,
+      paddingVertical : 18,
+      marginTop : 40,
+      width : '40%',
+      alignItems : 'center',
+     }} >
+      <Text style = {{
+        color : '#fff' ,
+        fontSize : 14 ,
+        fontWeight : 'bold',
+      }}> Order now </Text>
+     </TouchableOpacity>
+      
     </View>
   );
 }
